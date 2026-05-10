58cd9090603cf1d1eba66d4c1237f242eaed37df
SlateGray <mrslategray@protonmail.com>
2026-05-10 06:02:14 -0500

    remove dead custom-messages.ts
    file was unimported and only contained a duplicate navigation declaration
    already present in NavigationMessage.ts. Leftover from ContinueMessage removal.

--- src/messages/custom-messages.ts
+++
@@ -1,8 +0,0 @@
-import type { NavigationMessage } from "./NavigationMessage.js";
-
-// Extend CustomAgentMessages interface via declaration merging
-declare module "@earendil-works/pi-agent-core" {
-	interface CustomAgentMessages {
-		navigation: NavigationMessage;
-	}
-}
